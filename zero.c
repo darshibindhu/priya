4d90150a557c9b7c0e7c5e39131f7ab73f2481d9
darshibindhu <darshibindhu@gmail.com>
2016-10-25 10:33:18 +0530

    Create zero.c

---
+++ zero.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter the number");
+Scanf("%d",&a);
+if(a==0)
+{
+printf("The number is Zero");
+elseif(a<0)
+{
+printf("The number is Negative");
+}
+else
+{
+printf("The number is Positive");
+}
+}
+getch();
+}
+
+
