6e556ecd345f6abde804fee52a739f23d764e7cf
darshibindhu <darshibindhu@gmail.com>
2016-10-25 11:18:52 +0530

    Create Even.c

---
+++ Even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p;
+clrscr();
+printf("Enter the number");
+scanf("%d",&p);
+if(p%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("Odd");
+}
+}
